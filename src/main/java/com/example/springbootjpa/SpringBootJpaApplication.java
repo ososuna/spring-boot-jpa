2ab28b0e0b1ad5891b7739c1b4b75c6d313c9eae
Oswaldo Adrián Osuna <oswaldo.adrian@live.com.mx>
2022-02-15 22:33:07 -0600

    JPA repository

--- src/main/java/com/example/springbootjpa/SpringBootJpaApplication.java
+++ src/main/java/com/example/springbootjpa/SpringBootJpaApplication.java
@@ -1,13 +1,29 @@
 package com.example.springbootjpa;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringBootJpaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootJpaApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner commandLineRunner(StudentRepository studentRepository) {
+		return args -> {
+			var student = new Student();
+			student.setFirstName("Mitsuri");
+			student.setLastName("Kanroji");
+			student.setEmail("kanroji.mitsuri@test.com");
+			student.setAge(19);
+			System.out.println(student.getFirstName());
+			System.out.println(student.getLastName());
+			studentRepository.save(student);
+		};
 	}
 
 }
